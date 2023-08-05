fc9d5d52325835ee689cedd9ac0b3fc77233aafa
Ümit Uysal <42471170+umituysal@users.noreply.github.com>
2023-08-05 14:25:32 +0300

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -7,17 +7,7 @@ const Footer = () => {
       <div className="container flex flex-col items-center px-5 py-8 mx-auto sm:flex-row">
         <a className="flex items-center justify-center font-medium text-gray-900 title-font md:justify-start">
           <span className="ml-3 text-xl">Ecommerce</span>
-        </a>
-        <p className="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0">
-          <a
-            href="https://twitter.com/knyttneve"
-            className="ml-1 text-gray-600"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            @umituysal
-          </a>
-        </p>
+        </a>s
         <span className="inline-flex justify-center mt-4 sm:ml-auto sm:mt-0 sm:justify-start">
           <a className="text-gray-500"></a>
           <a className="ml-3 text-gray-500"></a>
